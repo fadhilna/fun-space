ca9ac94e38d96f35b430ee35c3a599b3833051d6
fadhil <nafadhil26@gmail.com>
2024-09-10 11:18:15 +0700

    Add files via upload
    rumus luas bangun

---
+++ app.py
@@ -0,0 +1,8 @@
+
+def luas_segitiga():
+    a  =int(input("masukan alas segitiga: "))
+    t  =int(input("masukan tinggi segitiga: "))
+    luas =  a * t/ 2
+    print("luas segitiga adalah : ",luas)
+
+luas_segitiga()
